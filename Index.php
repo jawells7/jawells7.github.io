411371db4968a0b37591d4c2cc334f9888345244
jakewells <99765399+jawells7@users.noreply.github.com>
2024-02-26 18:00:47 -0700

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,36 @@
+<html>
+
+<head>
+<?php include './includes/IndexHeader.css';?>
+<title>Jake Wells</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="Index.css">
+<style>
+img {
+	border-radius:50%;
+	width: 550px;
+	height: 500px;
+	clear:both;
+	padding-bottom: 10px;
+	
+}
+</style>
+
+</head>
+
+<body>	
+	<div class="main">
+		
+		<div class="image">
+			<img border="0" src="img/test_image.jpg">
+		</div>
+		
+		<div class="textbox">
+			<p>Hi, I'm Jake!</p>
+			<p>I'm a recent business Information Systems graduate with a passion for analysis and desire to be of service. Want to see more of my experience and education? Check out the "Projects", "Education", and "Work" pages above.</p>
+			<p>Would you like to get in touch with me? Click on the <i>Contact</i> button in the menu to email me. You can also find my LinkedIn by clicking on the <i>LinkedIn</i> button.</p>
+		</div>	
+	</div>
+
+</body>
+</html>
